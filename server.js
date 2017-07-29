3f019cfedecc2fe67befad92a37fb6f8a000f510
Won Maung Thein <wonmaungthein@gmail.com>
2017-07-29 16:04:38 +0100

    I have created a node express workshop 2

--- server.js
+++ server.js
@@ -1,9 +1,57 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
+const fs = require('fs');
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
+app.use(express.static("public", { 'extensions': ['html'] }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+let subheading = " My Happy Study";
+
+app.get('/', function (req, res) {
+    const filePath = __dirname + '/data/posts.json';
+    const callbackFunction = function(error, file) {
+        // we call .toString() to turn the file buffer to a String
+        const fileData = file.toString();
+        // we use JSON.parse to get an object out the String
+        const postsJson = JSON.parse(fileData);
+        // send the json to the Template to render
+        res.render('index', {
+          title: 'Etzali Profile', // insert your name instead
+          posts: postsJson
+        });
+    };
+    fs.readFile(filePath, callbackFunction);
+});
+
+
+app.get('/my-cv', function (req, res) {
+  res.render('my-cv', {
+    // title: title,
+    subheading: "My Work Experience"
+
+  });
+});
+
+app.get('/admin', function (req, res) {
+  res.render('admin', {
+    // title: title,
+    subheading: subheading
+  });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    title: "Contact me",
+    subheading: subheading
+
+  });
+});
+
+
 
 
 // what does this line mean: process.env.PORT || 3000
