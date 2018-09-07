c3cca21efe13c01e99b772ec7618e41b2c80f98e
Won Maung Thein <wonmaungthein@gmail.com>
2018-09-07 21:50:54 +0100

    Styling and indenting

--- helpers/readPosts.js
+++ helpers/readPosts.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const filePath = __dirname + '/../data/posts.json';
+const filePath = __dirname + "/../data/posts.json";
 /*
     To use this function, pass a callback that takes error as a first parameter and the postJSON as the second one
     i.e. readPosts((error, posts) => {
@@ -11,22 +11,21 @@ const filePath = __dirname + '/../data/posts.json';
         }
     })
 */
-const readPosts = (callback) => {
-    fs.readFile(filePath, (error, fileBuffer) => {
-        if(error) {
-            console.error(error);
-            return callback(error);
-        }
-        // Get the string out of the file buffer
-        const file = fileBuffer.toString();
+const readPosts = callback => {
+  fs.readFile(filePath, (error, fileBuffer) => {
+    if (error) {
+      console.error(error);
+      return callback(error);
+    }
+    // Get the string out of the file buffer
+    const file = fileBuffer.toString();
 
-        // convert to an object
-        const posts = JSON.parse(file);
+    // convert to an object
+    const posts = JSON.parse(file);
 
-        // return the object to the callback
-        callback(error, posts);
-    });
-}
+    // return the object to the callback
+    callback(error, posts);
+  });
+};
 
 module.exports = readPosts;
-
